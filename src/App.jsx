b36bec22595687cbc89feb25a005b8489ef08ed6
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-04-08 13:31:57 +0530

    Add Basic Fundamentals

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+//  function App(){
+//   const name = "Manvendra"
+//   const age = "20"
+//   console.log(name)
+//   const newArray = [1,2,34,5]
+//   const object = {
+//     name:"Manvendra",
+//     age:"12"
+//   }
+//   function Hello(e){
+// console.log(e.nativeEvent)
+//   }
+//   return (
+//    <>
+//     <h1>Manvendra</h1>
+//     <h1>sma</h1>
+//     <h1>{name}</h1>
+//     <h1>{age}</h1>
+//     <h1>{newArray[0]}</h1>
+//     <h1>{newArray[1]}</h1>
+//     <h1>{newArray}</h1>
+//     <h1>{object.name}</h1>
+//     {/* <button onClick={Hello}>Click</button> // Function Refrence
+//     <button onClick={Hello()}>Click</button> // Function Call */}
+//     <button onClick={(e)=>Hello(e)}>Click Pass</button> 
+//    </>
+//   )
+// }
+// export default App
+// // 1. The name of the function is same as file name (For better Pratice)
+// // 2. The name of the function starts with Capital letter always
+// // 3. it returns JSX 
+// // 4. It return parent element
+
+
